c730aaa9944c604ed2ef5cd037b5910497bf5c3c
XanthiTsagka <cs04817@uoi.gr>
2024-10-22 13:27:34 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -10,8 +10,22 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+    add s0, zero, zero
+    add t1, a1, zero
+    beq s0, a1, done
+    loop:    
+        lw t0, 0(a0)
+        beq t0, a2, exit_found
+        beq t1, zero, done
+    next:
+        addi a0, a0, 4
+        addi t1, t1, -1
+        j    loop
+    exit_found:
+        add  s0, a0, zero
+        bne t1, zero, next
+        j done
+
 #-----------------------------
 done:
     addi a7, zero, 10 
